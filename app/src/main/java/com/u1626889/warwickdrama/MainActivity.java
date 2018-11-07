c7b2edb596a1d579b73c9559c22a952b540d9a9f
Jack Piper <u1626889@warwick.ac.uk>
2018-11-07 18:59:00 +0000

    Made the cardviews fully programatic and added function to expand/collapse when the button is clicked.

--- app/src/main/java/com/u1626889/warwickdrama/MainActivity.java
+++ app/src/main/java/com/u1626889/warwickdrama/MainActivity.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity
     public ArrayList<String> typeArr = new ArrayList<String>();
     public ArrayList<String> societyArr = new ArrayList<String>();
     public ArrayList<String> descArr = new ArrayList<String>();
-    private Context mContext;
+    public ArrayList<Boolean> expandArr = new ArrayList<Boolean>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,87 +55,91 @@ public class MainActivity extends AppCompatActivity
         idArr.add(2);
         idArr.add(3);
         idArr.add(4);
+        idArr.add(5);
+        idArr.add(6);
+        idArr.add(7);
+        idArr.add(8);
         titleArr.add("WITS Presents: A Play auditions");
-        titleArr.add("oi get fucked i kiss dogs");
-        titleArr.add("Isn't it weird");
-        titleArr.add("My Minecraft account got deleted");
+        titleArr.add("Rent fundraiser!");
+        titleArr.add("A View From The Bridge");
+        titleArr.add("Producer needed! Behind the Eyes");
+        titleArr.add("WUDS and improv and you");
+        titleArr.add("Freshblood Views The Bridge!");
+        titleArr.add("Death Of An Anarchist auditions");
+        titleArr.add("Be on the DC panel!");
         typeArr.add("Audition");
-        typeArr.add("Misc");
+        typeArr.add("Social");
+        typeArr.add("Show");
+        typeArr.add("Production Opportunity");
         typeArr.add("Workshop");
-        typeArr.add("Misc");
+        typeArr.add("Social");
+        typeArr.add("Audition");
+        typeArr.add("Announcement");
         societyArr.add("WITS");
         societyArr.add("MTW");
+        societyArr.add("WUDS");
         societyArr.add("Codpeice");
+        societyArr.add("WUDS");
         societyArr.add("Freshblood");
+        societyArr.add("WUDS");
+        societyArr.add("Other");
         descArr.add("Come audition for Warwick Improv's edinburgh show, tentatively entitled 'A Play'! The audition will consist of a series of short form games.");
-        descArr.add("oi get fucked i kiss dogs");
-        descArr.add("Isn't it weird how you feet smell and your nose runs but your dad doesn't love you");
-        descArr.add("My Minecraft account got deleted and now I can't even watch youtube song parodies without crying");
+        descArr.add("Rent is having a fundraiser, come support this terrible show. Try not to think about how Angel kills dogs.");
+        descArr.add("A View From The Bridge is a crackin ply with accents and a chair. Come see.");
+        descArr.add("behind the eyes needs an assistant producer, and that assistant producer that behind the eyes needs, it could be you.");
+        descArr.add("WUDS are running a worskhop on improv, and they've used WITS as a way of injecting a bit of that.");
+        descArr.add("Your lovely freshblood social secs are gonna be at the duck at 6pm practicing our accents before jetting off to see WUDS' latest production - A View From The Bridge!");
+        descArr.add("Be an actor! Not an acorn. Act as the lead. Or as a support. Yay.");
+        descArr.add("Would YOU like to be the sole arbiter of people's hopes and dreams? Well, this is the next best thing I promise.");
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
+        expandArr.add(false);
 
 
+        for(int i = 0; i < idArr.size(); i++) {
+            CardView card = new CardView(this);
+            ConstraintLayout currentLayout = (ConstraintLayout) findViewById(R.id.cardViewLayout);
 
-
-        // Get the application context
-        mContext = getApplicationContext();
-
-        TextView tv = (TextView)findViewById(R.id.info_text);
-        tv.setText(titleArr.get(0));
-
-        final ConstraintLayout mConstraintLayout;
-
-        ImageButton button = findViewById(R.id.imageButton);
-        mConstraintLayout = (ConstraintLayout) findViewById(R.id.cardViewLayout);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                for(int i = 0; i < idArr.size(); i++) {
-                    CardView card = new CardView(mContext);
-
-                    // Set the CardView layoutParams
-                    ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
-                            ConstraintLayout.LayoutParams.MATCH_PARENT,
-                            ConstraintLayout.LayoutParams.WRAP_CONTENT
-                    );
-                    params.setMargins(15, 15, 15, 15);
-                    if(i == 0) {
-                        params.topToBottom = R.id.card_view9;
-                    } else {
-                        params.topToBottom = idArr.get(i-1);
-                    }
-
-                    card.setLayoutParams(params);
-
-                    // Set CardView corner radius
-                    card.setRadius(4);
-
-                    // Set cardView content padding
-                    card.setContentPadding(15, 15, 15, 15);
-
-                    // Set a background color for CardView
-                    card.setCardBackgroundColor(Color.LTGRAY);
-
-                    // Set the CardView maximum elevation
-                    card.setMaxCardElevation(15);
-
-                    // Set CardView elevation
-                    card.setCardElevation(9);
-
-                    card.setId(idArr.get(i));
-
-                    // Initialize a new TextView to put in CardView
-                    TextView tv = new TextView(mContext);
-                    tv.setLayoutParams(params);
-                    tv.setText(societyArr.get(i) + "\n"+ typeArr.get(i) + "\n\n" + titleArr.get(i) + "\n\n" + idArr.get(i));
-                    tv.setTextColor(Color.RED);
-
-                    // Put the TextView in CardView
-                    card.addView(tv);
-
-                    // Finally, add the CardView in root layout
-                    mConstraintLayout.addView(card);
-                }
-
+            // Set the CardView layoutParams and define how it will be arranged on the screen
+            ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(30, 30, 30, 30);
+            if(i == 0) {
+                params.topToTop = currentLayout.getId();
+            } else {
+                params.topToBottom = idArr.get(i-1);
             }
-        });
+            card.setLayoutParams(params);
+            // Set CardView corner radius
+            card.setRadius(4);
+            // Set cardView content padding
+            card.setContentPadding(15, 15, 15, 15);
+            // Set a background color for CardView
+            card.setCardBackgroundColor(Color.parseColor("#0dd6dd"));
+            // Set the CardView maximum elevation
+            card.setMaxCardElevation(15);
+            // Set CardView elevation
+            card.setCardElevation(9);
+            // THE FUNCTION View.generateViewId() WILL BE USED WHEN THE POST IS CREATED, SO idArr WILL HAVE A UNIQUE ID ALREADY, DON'T WORRY.
+            card.setId(idArr.get(i));
+            card.setTag("cardView"+idArr.get(i));
+
+
+            ConstraintLayout insideLayout = makeCardInside(idArr.get(i),titleArr.get(i),typeArr.get(i),societyArr.get(i),descArr.get(i));
+
+
+            card.addView(insideLayout);
+
+            // Finally, add the CardView in the correct layout
+            currentLayout.addView(card);
+        }
 
     }
 
@@ -178,6 +184,74 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // This method creates the layout that will go inside a CardView, given the required data
+    public ConstraintLayout makeCardInside(final int cardid, final String title, final String type, final String society, final String desc) {
+
+        // Define a new layout
+        // This is the layout inside the cardview. All the stuff is in here, then this is added to the cardview
+        final ConstraintLayout insideLayout = new ConstraintLayout(this);
+        // Need to generate a new unique ID for the layout
+        int newid = View.generateViewId();
+        insideLayout.setId(newid);
+
+        // Create the layout parameters for each view including the layout
+        final ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        // Create the layout parameters for each view including the layout
+        final ConstraintLayout.LayoutParams IMGparams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        // Create the layout parameters for each view including the layout
+        final ConstraintLayout.LayoutParams TVparams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        insideLayout.setLayoutParams(params);
+
+        ImageButton button = new ImageButton(this);
+        button.setBackgroundResource(R.drawable.ic_menu_send);
+        // Define the layout parameters for the image button
+        IMGparams.height = 120;
+        IMGparams.width = 120;
+        IMGparams.endToEnd = insideLayout.getId();
+        IMGparams.topToTop = insideLayout.getId();
+        button.setLayoutParams(IMGparams);
+
+
+        // Initialize a new TextView to put in CardView
+        final TextView tv = new TextView(this);
+        // Need to generate a new unique ID for the view
+        newid = View.generateViewId();
+        tv.setId(newid);
+        tv.setText(society + "\n"+ type + "\n\n" + title);
+        tv.setTextColor(Color.BLACK);
+        TVparams.endToStart = button.getId();
+        tv.setLayoutParams(TVparams);
+
+        // Add an onclick event so that clicking the button expands and collapses the text
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int arrayNum = idArr.indexOf(cardid);
+                tv.setText(" "+arrayNum);
+                if(expandArr.get(arrayNum) == false) {
+                    tv.setText(society + "\n"+ type + "\n\n" + title + "\n\n" + desc);
+                    expandArr.set(arrayNum,true);
+                } else {
+                    tv.setText(society + "\n"+ type + "\n\n" + title);
+                    expandArr.set(arrayNum,false);
+                }
+            }
+        });
+
+        // Put the TextView in CardView
+        insideLayout.addView(tv);
+        insideLayout.addView(button);
+        return insideLayout;
     }
 
 }
