61d15cd7343d0fe276ca923deb2565e3fc82dac7
Jack Piper <u1626889@warwick.ac.uk>
2019-01-23 15:57:24 +0000

    Implemented stemming and started tf-idf

--- app/src/main/java/com/u1626889/warwickdrama/PostComparator.java
+++ app/src/main/java/com/u1626889/warwickdrama/PostComparator.java
@@ -3,9 +3,9 @@ package com.u1626889.warwickdrama;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class PostComparator implements java.util.Comparator<Post>{
 
@@ -22,16 +22,28 @@ public class PostComparator implements java.util.Comparator<Post>{
         SharedPreferences prefs = context.getSharedPreferences("user_tags",0);
         String tags = prefs.getString("user_tags","").toLowerCase();
 
+        tags = stemSentence(tags);
+
+        String post1desc = post1.getDesc().toLowerCase();
+        post1desc = stemSentence(post1desc);
+        String post2desc = post2.getDesc().toLowerCase();
+        post2desc = stemSentence(post2desc);
+
         String post1tags = post1.getTags().toLowerCase();
         String post2tags = post2.getTags().toLowerCase();
 
-        int score1 = tagsCompare(post1tags, tags);
-        int score2 = tagsCompare(post2tags, tags);
+        int score1 = tagsCompare(post1desc, tags);
+        int score2 = tagsCompare(post2desc, tags);
+
+//        String leet = stemSentence("Hello, I'm jack! absolutely lovely to meet you, today we're going to replacing you without yeet");
+
+
+
 
         if(score1 > score2) return -1;
         else if(score1 < score2) return 1;
         else {
-            //                  TIE-BREAKER ONE
+            //                  TIE-BREAKER?
             return 0;
         }
     }
@@ -40,6 +52,9 @@ public class PostComparator implements java.util.Comparator<Post>{
         ArrayList<String> first = new ArrayList<>(Arrays.asList(t1.split(",")));
         ArrayList<String> second = new ArrayList<>(Arrays.asList(t2.split(",")));
 
+        Log.d("thing","first is "+first.toString());
+        Log.d("thing","second is '"+second.get(0)+"'");
+
         int score = 0;
 
         for(int i = 0; i < first.size(); i++) {
@@ -47,7 +62,7 @@ public class PostComparator implements java.util.Comparator<Post>{
                 score++;
             }
         }
-
+        Log.d("thing",""+score);
         return score;
     }
 
@@ -61,6 +76,44 @@ public class PostComparator implements java.util.Comparator<Post>{
     @Override
     public boolean equals(Object post1) {
         return true;
+    }
+
+    public String stemSentence(String text) {
+
+//        Log.d("thing","Initial value: " +text);
+
+
+        // make it lower case
+        text = text.toLowerCase();
+        // hack to keep dashed words (e.g. "non-specific" rather than "non" and "specific")
+        text = text.replaceAll("-+", "-0");
+        // replace any punctuation char but apostrophes and dashes by a space
+        text = text.replaceAll("[\\p{Punct}&&[^'-]]+", " ");
+        // replace most common english contractions
+        text = text.replaceAll("(?:'(?:[tdsm]|[vr]e|ll))+\\b", "");
+
+
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(text.split(" ")));
+        wordsList.removeAll(Collections.singleton(""));
+
+//        Log.d("thing","After prep: " +wordsList);
+
+
+        String stemmedSentence = "";
+
+        Stemmer stemmer;
+        for(int i = 0; i < wordsList.size(); i++) {
+            stemmer = new Stemmer();
+            stemmer.add(wordsList.get(i).toCharArray(), wordsList.get(i).length());
+            stemmer.stem();
+            stemmedSentence += " "+stemmer.toString();
+
+        }
+
+//        Log.d("thing","Finally: "+stemmedSentence);
+
+
+        return stemmedSentence;
     }
 
 }
